8ee0bfe9537ba3d12b141981b5cfebac744b6afb
TishanS <94636881+TishanS@users.noreply.github.com>
2022-05-01 11:05:47 +0530

    Update ProductAction.js

--- src/Redux/Actions/ProductAction.js
+++ src/Redux/Actions/ProductAction.js
@@ -2,26 +2,6 @@ import axios from "axios";
 import fakeStoreApi from "../../Api/fakeStoreApi";
 import { ActionTypes } from "../Constants/Action-Type";
 
-
-// export const fetchProducts =  () => {
-
-//   return async function (dispatch, getSatte){
-//     const response = await fakeStoreApi.get("/products");
-//     dispatch({type: ActionTypes.FETCH_PRODUCTS, payload: response.data});
-//   }; 
-// };
-
-// export const fetchProduct =  (id) => {
-
-//   return async function (dispatch, getSatte){
-//     const response = await fakeStoreApi.get(`/products/${id}`);
-//     dispatch({type: ActionTypes.SELECTED_PRODUCT, payload: response.data});
-//   }; 
-
-// };
-
-
-
 export const setProducts = (products) => {
   return {
     type: ActionTypes.SET_PRODUCTS,

b036a7156b8ba5e1493a65dc17fe6dc1173894de
TishanS <94636881+TishanS@users.noreply.github.com>
2022-05-01 11:12:24 +0530

    Update Header.js

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="ui fixed menu">
-      <div className="ui container " style={{backgroundColor: "lightpink"}}>
+      <div className="ui container " style={{backgroundColor: "lightGrey"}}>
         <h2>FakeStore</h2>
       </div>
     </div>

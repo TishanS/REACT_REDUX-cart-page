f7f7adc7eabce33273d497935a4dc7003cb77807
TishanS <94636881+TishanS@users.noreply.github.com>
2022-05-01 11:06:44 +0530

    Update ProductReducer.js

--- src/Redux/Reducer/ProductReducer.js
+++ src/Redux/Reducer/ProductReducer.js
@@ -2,15 +2,12 @@ import { ActionTypes } from "../Constants/Action-Type";
 
 const intialState = {
   products: []
- 
 };
 
 export const productsReducer = (state = intialState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
       return { ...state, products: payload };
-      // case ActionTypes.FETCH_PRODUCTS:
-      // return { ...state, products: payload };
     default:
       return state;
   }
